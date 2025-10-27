f9b4ce55b4cf488be47ffac583bef63ddb772850
bioinformaticsguy <bioinformaticsguy@gmail.com>
2025-10-27 12:31:29 +0100

    Update run_annot_talos.sh to load modules and correct pedigree file path

--- run_annot_talos.sh
+++ run_annot_talos.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-nextflow -c nextflow/annotation.config run nextflow/annotation.nf --large_files large_files/ --input_vcf_dir data/
+module load singularity/v4.1.3
+module load nextflow/v24.04.3
 
-nextflow -c nextflow/talos.config run nextflow/talos.nf --matrix_table nextflow/cohort_outputs/cohort.mt --pedigree data/pedgree.ped
+# nextflow -c nextflow/annotation.config run nextflow/annotation.nf --large_files large_files/ --input_vcf_dir nextflow/inputs/individual_vcfs
+# nextflow -c nextflow/talos.config run nextflow/talos.nf --matrix_table nextflow/cohort_outputs/cohort.mt --pedigree nextflow/inputs/pedigree.ped
+
+
+
+# nextflow -c nextflow/annotation.config run nextflow/annotation.nf --large_files large_files/ --input_vcf_dir data_back
+nextflow -c nextflow/talos.config run nextflow/talos.nf --matrix_table nextflow/cohort_outputs/cohort.mt --pedigree ped_files/pedigree.ped
